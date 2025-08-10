7f49c11b2a97fe0ee16d52508c87aa6820f1293a
ReinhardOL <reinhard@gmx.net>
2025-08-10 14:27:58 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE = 'zeugnisnoten-v1.2';
+const CACHE = 'zeugnisnoten-v1.2.1';
 const ASSETS = [
   './',
   './index.html',
@@ -11,11 +11,13 @@ self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
 self.addEventListener('activate', e => {
-  e.waitUntil(self.clients.claim());
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    .then(() => self.clients.claim())
+  );
 });
 self.addEventListener('fetch', e => {
   const url = new URL(e.request.url);
-  // Cache-first for same-origin requests
   if (url.origin === self.location.origin) {
     e.respondWith(
       caches.match(e.request).then(res => res || fetch(e.request).then(resp => {
